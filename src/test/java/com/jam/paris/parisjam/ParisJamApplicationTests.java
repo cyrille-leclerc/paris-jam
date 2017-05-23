a6cf3e82c330dfa14e87509873105094455ca2f9
Cyrille Le Clerc <cleclerc@cloudbees.com>
2017-05-23 20:32:56 +0200

    Add attachments

--- src/test/java/com/jam/paris/parisjam/ParisJamApplicationTests.java
+++ src/test/java/com/jam/paris/parisjam/ParisJamApplicationTests.java
@@ -5,6 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import static junit.framework.TestCase.assertTrue;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ParisJamApplicationTests {
@@ -18,4 +24,19 @@ public class ParisJamApplicationTests {
 	public void contextLoads() {
 	}
 
+	@Test
+	public void testApp() throws IOException {
+
+		assertTrue(true);
+		File myattachment = new File("myattachment");
+
+		myattachment.createNewFile();
+		FileWriter fileWriter = new FileWriter(myattachment);
+		try {
+			fileWriter.append("This is a JUnit-attachment sample");
+			System.out.println("[[ATTACHMENT|" + myattachment.getAbsolutePath() + "]]");
+		} finally {
+			fileWriter.close();
+		}
+	}
 }
